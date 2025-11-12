ddb404f7378bc4d2731a52bf32ef46bf624cdc81
MohamedAbdelmawgood16002100 <mohamed.abdelmawgood@student.giu-uni.de>
2025-11-12 15:26:53 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -3,7 +3,22 @@
 
 #include <string>
 using namespace std;
-
+class Visitor{
+    private:
+        string visitorName;
+        int ticketsBought;
+    public:
+        Visitor(string visitorName, int ticketsBought){
+            this->visitorName = visitorName;
+            this->ticketsBought = ticketsBought;
+        }
+        void displayInfo(){
+            cout<<"Visitor info:"<<endl;
+            cout<<"Name: "<<visitorName<<endl;
+            cout<<"Tickets bought: "<<ticketsBought<<endl;
+        }
+        ~Visitor(){};
+};
 //define visitor class here
 
 
