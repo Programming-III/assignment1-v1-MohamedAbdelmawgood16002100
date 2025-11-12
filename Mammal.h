266b133d5f5bae9e9fd7d168a633ad5ab0f9a3bd
MohamedAbdelmawgood16002100 <mohamed.abdelmawgood@student.giu-uni.de>
2025-11-12 15:28:39 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -2,7 +2,15 @@
 #define MAMMAL_H
 
 #include "Animal.h"
-
+class Mammal : public Animal{
+    private:
+        string furColor;
+    public:
+        Mammal(string name, int age, bool isHungry, string furColor):Animal(name, age, isHungry){
+        this -> furColor = furColor;
+        }
+        ~Mammal(){};
+};
 //define mammal class here
 
 
