266582978d3ce252b3ac42c7f66fa2f37a575760
MohamedAbdelmawgood16002100 <mohamed.abdelmawgood@student.giu-uni.de>
2025-11-12 15:29:08 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -2,7 +2,31 @@
 #define ENCLOSURE_H
 
 #include "Animal.h"
-
+class enclosure{
+    private:
+        Animal* animals;
+        int capacity;
+        int currentCount;
+    public:
+        enclosure(Animal* animals, int capacity, int currentCount){
+            this->animals = animals;
+            this->capacity = capacity;
+            this->currentCount = currentCount;
+        }
+        void addAnimal(Animal* a){
+            if (currentCount < capacity){
+                animals[currentCount] = *a;
+                currentCount++;
+            }else{
+                cout<<"Maximum capacity of Enclosure reached."<<endl;
+            };
+        }
+        void displayAnimals(){
+            for (int i = 0; i < currentCount; i++){
+                cout<<animals[i].getName()<<" ("<<animals[i].getAge()<<", "<<animals[i].getIsHungry()<<")"<<endl;
+            };
+        }
+};
 //define enclosure class here 
 
 
