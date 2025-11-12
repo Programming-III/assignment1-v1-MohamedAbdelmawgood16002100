6360a59ec1a16893a3f286654d2d66c9593c7501
MohamedAbdelmawgood16002100 <mohamed.abdelmawgood@student.giu-uni.de>
2025-11-12 15:22:49 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -3,8 +3,40 @@
 
 #include <string>
 using namespace std;
+class Animal{
+    private:
+        string name;
+        int age;
+        bool isHungry;
+    public:
+        Animal(string name, int age, bool isHungry){
+            this->name = name;
+            this->age = age;
+            this->isHungry = isHungry;
+        }
+        void display(){
+            cout << "Name: " << name << "Age: " << age << " Hungry: " << (isHungry ? "Yes" : "No") << endl;
+        }
+        void feed(){
+            if (isHungry = false){
+                cout<< name << "is not hungry."<<endl;
+            }
+            else{
+                cout<< name << "is Hungry."<<endl;
+            }
+        }
+        string getName(){
+            return name; 
+        }
+        int getAge(){
+            return age;
+        }
+        bool getIsHungry(){
+            return isHungry;
+        }
 
-//define animal class here 
+};
+//define animal class here
 
 
 
