c7b9b0cb3d0dd59fce246ef1840c5de313ac1c57
MohamedAbdelmawgood16002100 <mohamed.abdelmawgood@student.giu-uni.de>
2025-11-12 15:29:49 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -2,7 +2,15 @@
 #define BIRD_H
 
 #include "Animal.h"
-
+class Bird : public Animal{
+    private:
+        float wingSpan;
+    public:
+        Bird(string name, int age, bool isHungry, float wingSpan):Animal(name, age, isHungry){
+            this -> wingSpan = wingSpan;
+        }
+        ~Bird(){};
+};
 //define class bird here 
 
 
