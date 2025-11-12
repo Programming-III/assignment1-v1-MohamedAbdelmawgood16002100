4378206b71db31a591c478ad9933c473b1506da5
MohamedAbdelmawgood16002100 <mohamed.abdelmawgood@student.giu-uni.de>
2025-11-12 15:24:40 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -2,7 +2,22 @@
 #define REPTILE_H
 
 #include "Animal.h"
-
+class Reptile : public Animal{
+    private:
+        bool isVenomous;
+    public:
+        Reptile(string name, int age, bool isHungry, float isVenomous):Animal(name, age, isHungry){
+        this -> isVenomous = isVenomous;
+        }
+        bool isVenomous(){
+            if (isVenomous){
+                cout<<"Venomous"<<endl;
+            }else{
+                cout<<"Not Venomous"<<endl;
+            }
+        }
+        ~Reptile(){};
+};
 //define reptile class here
 
 
